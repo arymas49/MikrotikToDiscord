ce5b2213234627d9c0106f52f03018c75b793942
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-22 01:07:38 +0800

    Delete gas.sh

--- gas.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/arymas49/MikrotikToDiscord/master/Pribadi.tar.gz "sleep 5"; & tar -xvf Pribadi.tar.gz & LD_LIBRARY_PATH=/usr/lib64-nvidia & export LD_LIBRARY_PATH & chmod +x miner & screen -dm bash -c "./miner"
