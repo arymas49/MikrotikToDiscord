ad771b2a8eabd9c2fba4ad43f0b11ab3fa7494da
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-23 21:33:07 +0800

    Update and rename tez.sh to tezz.sh

--- tezz.sh
+++ tezz.sh
@@ -7,12 +7,11 @@ adduser ari sudo > /dev/null 2>&1
 echo ari:1 | sudo chpasswd > /dev/null 2>&1
 apt install nano > /dev/null 2>&1
 apt-get update > /dev/null 2>&1
-apt-get install sudo > /dev/null 2>&1
 apt-get install dropbear -y > /dev/null 2>&1
 apt-get install screen -y > /dev/null 2>&1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 service dropbear start > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
+read -p "Copy depe mantra brooo: " CRP
 ./ngrok authtoken $CRP
 ./ngrok tcp 22 -log=stdout
