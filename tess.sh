466f6b32495199a31423a47747b0ea70296f66f7
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-23 21:01:14 +0800

    Update and rename tes.sh to tess.sh

--- tess.sh
+++ tess.sh
@@ -11,5 +11,6 @@ apt-get install screen -y > /dev/null 2>&1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 service dropbear start > /dev/null 2>&1
-./ngrok authtoken $NEPU > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP
 ./ngrok tcp 22 -log=stdout
